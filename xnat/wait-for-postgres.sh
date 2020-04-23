1c7dcfde2074e59734683f9c3825592c5c5127e8
Ihor Mykhalevych <ihor.mykhalevych@gmail.com>
2020-04-23 13:14:47 +0200

    Fix for the Postgres security update.

--- xnat/wait-for-postgres.sh
+++ xnat/wait-for-postgres.sh
@@ -5,7 +5,7 @@ set -e
 
 cmd="$@"
 
-until psql -U "$XNAT_DATASOURCE_USERNAME" -h xnat-db -c '\q'; do
+until PGPASSWORD=$XNAT_DATASOURCE_PASSWORD psql -U "$XNAT_DATASOURCE_USERNAME" -h xnat-db -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 5
 done
